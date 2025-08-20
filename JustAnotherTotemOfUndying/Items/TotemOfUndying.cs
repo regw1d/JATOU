cb2700f054ae2ffa8cc5f39bad29cd357b1aa356
regw1d <regwid228@gmail.com>
2025-08-20 23:58:26 +0400

    Update TotemOfUndying.cs

--- JustAnotherTotemOfUndying/Items/TotemOfUndying.cs
+++ JustAnotherTotemOfUndying/Items/TotemOfUndying.cs
@@ -6,42 +6,38 @@ namespace JustAnotherTotemOfUndying.Items
 {
     public class TotemOfUndying : ModItem
     {
-        public override void SetStaticDefaults()
-        {
-        }
-
         public override void SetDefaults()
         {
-            Item.width = 16;
-            Item.height = 16;
-            Item.rare = (byte)ItemRarityID.Yellow;
-            Item.value = Item.sellPrice(gold: 10);
+            Item.width = 8;
+            Item.height = 8;
+            Item.rare = ItemRarityID.Yellow;
+            Item.value = Item.sellPrice(gold: 5);
             Item.maxStack = 1;
         }
 
         public override void AddRecipes()
         {
             CreateRecipe()
-                .AddIngredient(ItemID.GoldBar, 30)
+                .AddIngredient(ItemID.GoldBar, 10)
                 .AddIngredient(ItemID.LifeCrystal, 1)
-                .AddIngredient(ItemID.FragmentSolar, 4)
-                .AddIngredient(ItemID.FragmentVortex, 4)
-                .AddIngredient(ItemID.FragmentNebula, 4)
-                .AddIngredient(ItemID.FragmentStardust, 4)
-                .AddIngredient(ItemID.SoulofLight, 15)
-                .AddIngredient(ItemID.SoulofNight, 15)
+                .AddIngredient(ItemID.FragmentSolar, 2)
+                .AddIngredient(ItemID.FragmentVortex, 2)
+                .AddIngredient(ItemID.FragmentNebula, 2)
+                .AddIngredient(ItemID.FragmentStardust, 2)
+                .AddIngredient(ItemID.SoulofLight, 4)
+                .AddIngredient(ItemID.SoulofNight, 4)
                 .AddTile(TileID.LunarCraftingStation)
                 .Register();
 
             CreateRecipe()
-                .AddIngredient(ItemID.PlatinumBar, 30)
+                .AddIngredient(ItemID.PlatinumBar, 10)
                 .AddIngredient(ItemID.LifeCrystal, 1)
-                .AddIngredient(ItemID.FragmentSolar, 4)
-                .AddIngredient(ItemID.FragmentVortex, 4)
-                .AddIngredient(ItemID.FragmentNebula, 4)
-                .AddIngredient(ItemID.FragmentStardust, 4)
-                .AddIngredient(ItemID.SoulofLight, 15)
-                .AddIngredient(ItemID.SoulofNight, 15)
+                .AddIngredient(ItemID.FragmentSolar, 2)
+                .AddIngredient(ItemID.FragmentVortex, 2)
+                .AddIngredient(ItemID.FragmentNebula, 2)
+                .AddIngredient(ItemID.FragmentStardust, 2)
+                .AddIngredient(ItemID.SoulofLight, 4)
+                .AddIngredient(ItemID.SoulofNight, 4)
                 .AddTile(TileID.LunarCraftingStation)
                 .Register();
         }
